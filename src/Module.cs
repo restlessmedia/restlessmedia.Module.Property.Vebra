c5a77663d8bac6fa62af269285b2a682b16b0e6d
Philip Cooper <phillycooper@hotmail.com>
2020-06-10 14:44:08 +0100

    Updates and missing registration

--- src/Module.cs
+++ src/Module.cs
@@ -7,8 +7,9 @@ namespace restlessmedia.Module.Property.Vebra
   {
     public void RegisterComponents(ContainerBuilder containerBuilder)
     {
+      containerBuilder.RegisterType<ApiPropertyProvider>().As<IApiPropertyProvider>().SingleInstance();
       containerBuilder.RegisterType<ApiPropertyService>().As<IApiPropertyService>().SingleInstance();
-      containerBuilder.RegisterType<ApiPropertyDataProvider>().As<IApiPropertyDataProvider>().SingleInstance(); 
+      containerBuilder.RegisterType<ApiPropertyDataProvider>().As<IApiPropertyDataProvider>().SingleInstance();
     }
   }
 }
